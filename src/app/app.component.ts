5c6bc110d059779148ba853be30cdf14c7d60f09
wesse206 <wessel.steenkamp@gmail.com>
2022-06-28 15:41:57 +0200

    Basic form that displays name, email and number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+export class OppiHoek {
+  constructor(
+    public name: string,
+    public email: string,
+    public number: string,
+  ) { }
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Oppihoek';
+
+  model = new OppiHoek('', '', '');
+
+  submitted = false;
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.model)
+  }
+
 }
